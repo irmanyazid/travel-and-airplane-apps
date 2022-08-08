34ed03ff5c69c690e9fe66892fa3d5d30d6fce70
Irman Yazid <irmandizay@gmail.com>
2022-08-08 19:48:31 +0700

    add and change Sign Up Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 // import 'package:airplane/ui/pages/get_started_page.dart';
 import 'package:airplane/ui/pages/get_started_page.dart';
+import 'package:airplane/ui/pages/sign_up_page.dart';
 import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
-// import 'ui/pages/splash_page.dart';
+import 'ui/pages/splash_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => SplashPage(),
         '/get-started': (context) => GetStartedPage(),
+        '/sign-up': (context) => SignUpPage(),
       },
     );
   }
