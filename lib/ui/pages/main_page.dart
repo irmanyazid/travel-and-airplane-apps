fc011c97005037e684e30f1258dff283cf848058
Irman Yazid <irmandizay@gmail.com>
2022-08-09 17:57:17 +0700

    add mainpage.dart and buttom navigation

---
+++ lib/ui/pages/main_page.dart
@@ -0,0 +1,143 @@
+import 'package:airplane/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget customButtomNavigation() {
+      return Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          width: double.infinity,
+          height: 60,
+          margin: EdgeInsets.only(
+            bottom: 30,
+            left: defaultMargin,
+            right: defaultMargin,
+          ),
+          decoration: BoxDecoration(
+            color: kWhiteColor,
+            borderRadius: BorderRadius.circular(18),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/icon_home.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 30,
+                    height: 2,
+                    decoration: BoxDecoration(
+                      color: kPrimaryColor,
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/icon_booking.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 30,
+                    height: 2,
+                    decoration: BoxDecoration(
+                      color: kTransparantColor,
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/icon_card.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 30,
+                    height: 2,
+                    decoration: BoxDecoration(
+                      color: kTransparantColor,
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/icon_settings.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 30,
+                    height: 2,
+                    decoration: BoxDecoration(
+                      color: kTransparantColor,
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: Stack(
+        children: [
+          Text('Main Page'),
+          customButtomNavigation(),
+        ],
+      ),
+    );
+  }
+}
