fb3f2e62059023cb8158e3d76182c5d3205409ab
Irman Yazid <irmandizay@gmail.com>
2022-08-15 23:23:10 +0700

    add page transaction_page, wallet_page, setting_page and slicing other pages

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,5 +1,8 @@
 import 'package:airplane/shared/theme.dart';
-import 'package:airplane/ui/pages/home_page.dart';
+import 'package:airplane/ui/pages/setting_page.dart';
+// import 'package:airplane/ui/pages/home_page.dart';
+// import 'package:airplane/ui/pages/transaction_page.dart';
+// import 'package:airplane/ui/pages/wallet_page.dart';
 import 'package:airplane/ui/widgets/custom_bottom_navigation_item.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +12,7 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget buildContent() {
-      return HomePage();
+      return SettingPage();
     }
 
     Widget customButtomNavigation() {
