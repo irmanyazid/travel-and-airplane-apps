3ad184a477c5374a3d1390c7d87b5f0f3b962f23
Irman Yazid <irmandizay@gmail.com>
2022-08-18 10:43:18 +0700

    add signup auth service & user service

--- lib/cubit/auth_cubit.dart
+++ lib/cubit/auth_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:airplane/models/user_model.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,36 @@
+import 'package:airplane/models/user_model.dart';
+import 'package:airplane/services/user_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+//Class
+class AuthService {
+  //Object
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //Method
+  Future<UserModel> signUp(
+      {required String email,
+      required String password,
+      required String name,
+      String hobby = ''}) async {
+    try {
+      //NOTE: FIREBASE AUTHENTICATION
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      UserModel user = UserModel(
+          id: userCredential.user!.uid,
+          email: email,
+          name: name,
+          hobby: hobby,
+          balance: 280000000);
+
+      //NOTE: FIRESTORE
+
+      await UserService().setUser(user);
+      return user;
+    } catch (e) {
+      throw e;
+    }
+  }
+}
