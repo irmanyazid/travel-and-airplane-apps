53dbec454c99d88ba5c4ac46660138d8e5cee154
Irman Yazid <irmandizay@gmail.com>
2022-08-21 15:21:40 +0700

    add method fetch transactions

--- lib/models/transaction_model.dart
+++ lib/models/transaction_model.dart
@@ -3,6 +3,7 @@ import 'package:equatable/equatable.dart';
 
 //Parameter
 class TransactionModel extends Equatable {
+  final String id;
   final DestinationModel destination;
   final int amountOfTraveler;
   final String selectedSeats;
@@ -15,6 +16,7 @@ class TransactionModel extends Equatable {
 //Constructor
   TransactionModel({
     required this.destination,
+    this.id = '',
     this.amountOfTraveler = 0,
     this.selectedSeats = '',
     this.insurance = false,
@@ -23,6 +25,20 @@ class TransactionModel extends Equatable {
     this.price = 0,
     this.grandTotal = 0,
   });
+
+  factory TransactionModel.fromJson(String id, Map<String, dynamic> json) =>
+      TransactionModel(
+        destination: DestinationModel.fromJson(
+            json['destination']['id'], json['destination']),
+        id: id,
+        amountOfTraveler: json['amountOfTraveler'],
+        selectedSeats: json['selectedSeats'],
+        insurance: json['isurance'],
+        refundable: json['refundable'],
+        vat: json['vat'],
+        price: json['price'],
+        grandTotal: json['grandTotal'],
+      );
 
   @override
   List<Object?> get props => [
