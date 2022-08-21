77ec16d89b476224446c3ab3cd558f2f28ed1dab
Irman Yazid <irmandizay@gmail.com>
2022-08-21 14:54:05 +0700

    add transaction to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:airplane/ui/pages/main_page.dart';
 import 'package:airplane/ui/pages/sign_in_page.dart';
 import 'package:airplane/ui/pages/sign_up_page.dart';
 import 'package:airplane/ui/pages/splash_page.dart';
+import 'package:airplane/ui/pages/success_checkout_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -54,6 +55,7 @@ class MyApp extends StatelessWidget {
           '/sign-in': (context) => SignInPage(),
           '/bonus': (context) => BonusPage(),
           '/main': (context) => MainPage(),
+          '/success': (context) => SuccessCheckoutPage(),
         },
       ),
     );
